0b3aeefa8f22b8292a77113fc1b582dc141e78a7
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-23 22:11:22 +0530

    wip: figuring out how to extend protos to child funcs

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 
 import { z } from './z.js';
 
-console.log(z.string('aaaaaaa').min(4).max(5))
+console.log(z)
--- z.js
+++ z.js
@@ -6,17 +6,21 @@ export const Z = function () {
 }
 
 Z.prototype.string = function (data) {
-  try {
-    if (typeof data === 'string') {
-      this.data = data
-      return this;
-    } else throw new Error(`Error while parsing ${data}`)
-  } catch (error) {
-    console.log(error)
-  }
+  if (typeof data === 'string') {
+    this.data = data
+    return this;
+  } else throw new Error(`Error while parsing ${data}`)
 }
 
-Z.prototype.min = function (num) {
+
+
+export const P = function () {
+  this.data = Z.prototype
+  console.log(Z)
+
+}
+
+P.prototype.min = function (num) {
   if (this.data.length < num) {
     throw new Error(`Minimum length should is ${num}`)
   } else {
@@ -24,31 +28,13 @@ Z.prototype.min = function (num) {
   }
 }
 
-
-Z.prototype.max = function (num) {
+P.prototype.max = function (num) {
   if (this.data.length > num) {
     throw new Error(`Maximum length should is ${num}`)
   } else return this;
 }
 
-export const z = new Z()
-
-// export const z = {
-//   string: (data) => {
-//     try {
-//       if (typeof data === 'string') {
-//         return data;
-//       } else throw new Error(`Error while parsing ${data}`)
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   },
-//   min:(data) => {
-//     if(data.length > data){
-//       return data
-//     }
-//   }
-// }
+export const z = new P()
 
 
 
